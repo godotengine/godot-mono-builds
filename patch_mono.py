6236e2ee74d8cfa4d0ac543d5aa3a907864b7613
Rémi Verschelde <rverschelde@gmail.com>
2023-05-26 11:36:48 +0200

    CI: Mono 6.12.0.198, Python 3.11, use ubuntu-latest
    - Rediff patches against Mono 6.12.0.198.
    - Drop `xcode_13_14_fix.diff` which is included upstream in 6.12.0.198.

--- patch_mono.py
+++ patch_mono.py
@@ -34,7 +34,6 @@ def main(raw_args):
         'wasm_m2n_trampolines_hook.diff',
         'btls-cmake-args-linux-mingw.diff',
         'btls-cmake-arm64.diff',
-        'xcode_13_14_fix.diff',
         'mono-linux-disable-mmap-MAP_32BIT.diff',
     ]
 
